a670d92ba2c1bc6f8997fad7a482583baad0eca5
feztix <pan0v.s.s@yandex.ru>
2020-12-03 09:26:43 +0300

    Stage 4/5: Create a new page to add news using a form. This form should send data via POST to /news/create/ where a new record will be created with two parameters, title and text. When the request is completed, the response should redirect you back to the main page

--- HyperNews Portal/task/hypernews/news/urls.py
+++ HyperNews Portal/task/hypernews/news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', ComingSoon.as_view()),
+    path('create/', CreateNews.as_view()),
     path('<int:link>/', OneNews.as_view()),
     path('', AllNews.as_view())
 ]
