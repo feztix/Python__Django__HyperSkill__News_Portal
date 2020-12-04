faf251c984e5893b45db6f00fb441a6b0055d83c
feztix <pan0v.s.s@yandex.ru>
2020-12-04 07:41:13 +0300

    Stage 5/5: Add a search form to your main page with one input element with the name q. The form should send GET requests to the same /news/ page. The format of the result page stays the same but includes only articles whose titles match the search term.

--- HyperNews Portal/task/hypernews/hypernews/urls.py
+++ HyperNews Portal/task/hypernews/hypernews/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from news.views import ComingSoon, Main
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('news.urls')),
-    path('', ComingSoon.as_view()),
-    path('news/', include('news.urls'))
+    path('news/', include('news.urls')),
+    path('', Main.as_view())
 ]
